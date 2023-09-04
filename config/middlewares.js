fe07a8a0eee941177663560ae8b9666ccca2f9b8
farizajju0804 <ajfariz13@gmail.com>
2023-09-05 02:28:31 +0530

    hiding article

--- config/middlewares.js
+++ config/middlewares.js
@@ -12,7 +12,7 @@ module.exports = [
         "http://localhost:1337",
         "http://localhost:3000",
         "http://example2",
-        "https://squid-app-vph52.ondigitalocean.app"
+        "https://king-prawn-app-hq64l.ondigitalocean.app"
       ],
     },
   },
