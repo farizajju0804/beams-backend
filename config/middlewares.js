358e31147921c809ce0d02e450115d64a8baecb7
farizajju0804 <ajfariz13@gmail.com>
2023-09-08 11:52:12 +0530

    db change check

--- config/middlewares.js
+++ config/middlewares.js
@@ -13,8 +13,8 @@ module.exports = [
         "http://localhost:3000",
         "http://example2",
         "https://king-prawn-app-hq64l.ondigitalocean.app",
-        "https://www.beams.world",
-        "https://beams-frontend.vercel.app"
+        "https://www.beams.world"
+        // "https://beams-frontend.verel.app"
 
       ],
     },
